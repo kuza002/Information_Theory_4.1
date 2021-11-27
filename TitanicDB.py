81479da2c62263c324b1dbda1cae9bd217c8d77f
kuza- <G9fw32l3>
2021-11-27 07:05:04 +0300

    Try to create validation

--- TitanicDB.py
+++ TitanicDB.py
@@ -7,15 +7,15 @@ from torch.utils.data import Dataset
 
 
 class TitanicDB(Dataset):
-    def __init__(self, path):
+    def __init__(self, path, device):
         # Initialize data, download, etc.
         # read with numpy or pandas
         xy = np.loadtxt(path, delimiter=',', dtype=np.float32, skiprows=1)
         self.n_samples = xy.shape[0]
 
         # here the first column is the class label, the rest are the features
-        self.x_data = torch.from_numpy(xy[:, 1:])  # size [n_samples, n_features]
-        self.y_data = torch.from_numpy(xy[:, [0]])  # size [n_samples, 1]
+        self.x_data = torch.from_numpy(xy[:, 1:]).to(device)  # size [n_samples, n_features]
+        self.y_data = torch.from_numpy(xy[:, [0]]).to(device)  # size [n_samples, 1]
 
     def __len__(self):
         return self.n_samples
--- train.py
+++ train.py
@@ -5,24 +5,9 @@ from torch.utils.data import DataLoader
 from TitanicDB import TitanicDB
 from logisticregression import LogisticRegression
 
-train_dataset = TitanicDB('dataset/training_set.csv')
-train_loader = DataLoader(dataset=train_dataset,
-                          batch_size=10,
-                          shuffle=True,)
 
-model = LogisticRegression(6)
-
-# 2) Loss and optimizer
-num_epochs = 100
-learning_rate = 0.01
-criterion = nn.BCELoss()
-optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-
-print(model(torch.tensor([3.0,0.0,22.0,1.0,0.0,7.25])))
-
-# 3) Training loop
-for epoch in range(num_epochs):
-    for inputs, labels in train_loader:
+def training_loop(model, data_loader, optimizer):
+    for inputs, labels in data_loader:
         # Forward pass and loss
         y_pred = model(inputs)
         loss = criterion(y_pred, labels)
@@ -34,7 +19,73 @@ for epoch in range(num_epochs):
         # zero grad before new step
         optimizer.zero_grad()
 
-        # if (epoch + 1) % 10 == 0:
-        #     print(f'epoch: {epoch + 1}, loss = {loss.item():.4f}')
+        print(f'epoch: {epoch + 1}, loss = {loss.item():.4f}')
 
-print(model(torch.tensor([3.0,0.0,22.0,1.0,0.0,7.25])))
+
+def test(model, train_dataset):
+    correct_answers = 0
+    with torch.no_grad():
+        for inputs, labels in train_dataset:
+            y_pred = model(inputs)
+            y_pred = round(float(y_pred))
+            if y_pred == labels:
+                correct_answers += 1
+    return correct_answers / len(train_dataset)
+
+
+if torch.cuda.is_available():
+    device = torch.device('cuda')
+else:
+    device = torch.device('cpu')
+
+train_dataset = TitanicDB('dataset/training_set.csv', device)
+test_dataset = TitanicDB('dataset/test_set.csv', device)
+validation_dataset = TitanicDB('dataset/validation_set.csv', device)
+
+model = LogisticRegression(6).to(device)
+
+# 2) Loss and optimizer
+num_epochs = 100
+criterion = nn.BCELoss()
+
+
+print("Accuracy:", test(model, test_dataset))
+
+# 3) Training loop
+
+max_accuracy = 0.0
+optimal_batch_size = 0
+optimal_learning_rate = 0
+optimal_model = None
+
+
+for batch_size in range(1, len(validation_dataset)):
+
+    train_loader = DataLoader(dataset=train_dataset,
+                              batch_size=batch_size,
+                              shuffle=True)
+
+    for learning_rate in range(5):
+        optimizer = torch.optim.SGD(model.parameters(), lr=10 ** -learning_rate)
+
+        for epoch in range(num_epochs):
+            training_loop(model, train_loader, optimizer)
+
+        accuracy = test(model, validation_dataset)
+        print("With batch size =", batch_size, 'and lr =', learning_rate)
+        print("Accuracy", accuracy, ' >', max_accuracy)
+        if accuracy > max_accuracy:
+            max_accuracy = accuracy
+            optimal_learning_rate = 10**-learning_rate
+            optimal_batch_size = batch_size
+            optimal_model = model
+            print("\nWith batch size =", optimal_batch_size, 'and lr =', optimal_learning_rate)
+            print("Max accuracy:", max_accuracy)
+
+        model = LogisticRegression(6).to(device)
+
+# 4) Test loop
+
+print("With batch size =", optimal_batch_size, 'and lr =', optimal_learning_rate)
+print("Accuracy:", max_accuracy)
+print(test(optimal_model, test_dataset))
